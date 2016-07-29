411a07aef6495ae6808a8be1cad7d10733162df0
陈少波 <chenshaobo65@gmail.com>
2016-07-28 18:54:28 -0700

    Initial Commit

---
+++ live/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  live
+//
+//  Created by swift on 16/7/28.
+//  Copyright © 2016年 chensb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

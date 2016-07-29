27a98d64e1b60a4296dd6b9c0017a7f39a478c88
swift <chenshaobo65@gmail.com>
2016-07-29 00:43:38 -0700

    add create room

--- live/AppDelegate.swift
+++ live/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var socket = Socket()
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
+        socket.getRoom()
         return true
     }
 
